a67d09efd77b341c3c5e5b2fc5e7d467b296ad84
joaowozniack <90704865+joaowozniack@users.noreply.github.com>
2023-08-10 16:19:18 -0300

    melhoria do css página inicial

--- public/js/inicio.js
+++ public/js/inicio.js
@@ -1,7 +1,9 @@
 document.getElementById("menuBtn").onclick = function() {
     document.getElementById("sideMenu").style.width = "250px";
+    document.getElementById("logoPrincipal").style.marginLeft = "220px"
   };
   
 document.getElementById("closeBtn").onclick = function() {
     document.getElementById("sideMenu").style.width = "0";
+    document.getElementById("logoPrincipal").style.marginLeft = "20px"
   };
