2e800b573bcc889215f4d6d711d76b552a9737d6
Arthur Prezotto Andrade <70661461+arthurprezottoandrade@users.noreply.github.com>
2023-08-09 21:07:41 -0300

    Barra lateral

---
+++ public/js/inicio.js
@@ -0,0 +1,7 @@
+document.getElementById("menuBtn").onclick = function() {
+    document.getElementById("sideMenu").style.width = "250px";
+  };
+  
+document.getElementById("closeBtn").onclick = function() {
+    document.getElementById("sideMenu").style.width = "0";
+  };
