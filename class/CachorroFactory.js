3c01310f6d2aeaba815f4d1efd0fa8344b4bc53f
Matheus Henrique Rosa <71407848+mhrrosa@users.noreply.github.com>
2023-09-25 14:14:07 -0300

    Update CachorroFactory.js

--- class/CachorroFactory.js
+++ class/CachorroFactory.js
@@ -1,9 +1,9 @@
 const CachorroDB = require('./CachorroDB');
-
+// classe responsavel pela criação da factory do cachorro
 class CachorroFactory {
     static criarCachorro({ nome, apelido, anoNascimento, porte, raca, situacao, imagem }) {
         return new CachorroDB(nome, apelido, anoNascimento, porte, raca, situacao, imagem);
     }
 }
-
+// exportando o modulo
 module.exports = CachorroFactory;
