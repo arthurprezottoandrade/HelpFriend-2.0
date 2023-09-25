c4fceaaa151ae319b24515d8503ee762213a2b44
Arthur Prezotto Andrade <70661461+arthurprezottoandrade@users.noreply.github.com>
2023-09-25 20:15:07 -0300

    Singleton

--- class/CachorroDB.js
+++ class/CachorroDB.js
@@ -1,8 +1,9 @@
 // importa a classe 'Database'
 const Database = require('./Database');
-class CachorroDB {
+class CachorroDB extends Database{
     // atributos necessarios para a criação do objeto
     constructor(nome, apelido, anoNascimento, porte, raca, situacao, imagem) {
+        super();
         this.nome = nome;
         this.apelido = apelido;
         this.anoNascimento = anoNascimento;
@@ -10,15 +11,13 @@ class CachorroDB {
         this.raca = raca;
         this.situacao = situacao;
         this.imagem = imagem;
-        // cria uma nova instância da classe Database
-        this.Database = new Database();
     }
     // método para inserir um novo cachorro no banco de dados
     inserirCachorro(dados, callback) {
         // inserir dados na tabela 'cachorro'
         const sql = 'INSERT INTO cachorro (Nome, Id_Raca, Ano_Nascimento, Porte, Adotado, Situacao, Id_Instituicao, Imagem, Apelido) VALUES (?, 1, ?, ?, \'nao\', ?, 1, ?, ?)';
         // executa a consulta de inserção SQL no banco de dados
-        this.Database.query(sql, [dados.nome, dados.anoNascimento, dados.porte, dados.situacao, dados.imagem, dados.apelido], callback);
+        this.query(sql, [dados.nome, dados.anoNascimento, dados.porte, dados.situacao, dados.imagem, dados.apelido], callback);
     }
 
     // método para buscar todos os cachorros no banco de dados
@@ -26,7 +25,7 @@ class CachorroDB {
         // busca dados na tabela 'cachorro'
         const sql = 'SELECT * FROM cachorro';
         // executa a consulta de busca SQL no banco de dados
-        this.Database.query(sql, callback);
+        this.query(sql, callback);
     }
 }
 
