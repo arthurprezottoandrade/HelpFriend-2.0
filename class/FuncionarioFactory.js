d8954f472e20905be3f230a2e6c3afad4ba0e54e
Matheus Henrique Rosa <71407848+mhrrosa@users.noreply.github.com>
2023-09-25 14:16:25 -0300

    Update FuncionarioFactory.js

--- class/FuncionarioFactory.js
+++ class/FuncionarioFactory.js
@@ -1,9 +1,9 @@
 const Funcionario = require('./FuncionarioDB');
-
+// classe responsavel pela fabrica de criação de funcionarios
 class FuncionarioFactory {
     static criarFuncionario({ nome, email, senha, cpf, cargo }) {
         return new Funcionario(nome, email, senha, cpf, cargo);
     }
 }
-
+// exportando modulos
 module.exports = FuncionarioFactory;
