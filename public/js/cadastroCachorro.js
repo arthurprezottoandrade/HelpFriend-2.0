aa974f298d5f8a52733d7021a8ccdd584d83babf
Arthur Prezotto Andrade <70661461+arthurprezottoandrade@users.noreply.github.com>
2023-09-25 20:54:16 -0300

    Upload de foto

--- public/js/cadastroCachorro.js
+++ public/js/cadastroCachorro.js
@@ -3,8 +3,21 @@ document.getElementById("menuBtn").onclick = function() {
     document.getElementById("logoPrincipal").style.marginLeft = "220px"
 };
 
-
 document.getElementById("closeBtn").onclick = function() {
     document.getElementById("sideMenu").style.width = "0";
     document.getElementById("logoPrincipal").style.marginLeft = "20px"
 };
+
+document.getElementById("foto").addEventListener("change", function() {
+    // Verifique se o arquivo foi carregado
+    if (this.files && this.files[0]) {
+        var reader = new FileReader();
+        
+        reader.onload = function(e) {
+            // Atualize a imagem
+            document.getElementById("dogImage").src = e.target.result;
+        };
+        
+        reader.readAsDataURL(this.files[0]);
+    }
+});
