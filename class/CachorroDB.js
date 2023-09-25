56b4431122bb185dafd38c09a8c0cc8f61a34165
Matheus Henrique Rosa <71407848+mhrrosa@users.noreply.github.com>
2023-09-25 14:15:12 -0300

    Update CachorroDB.js

--- class/CachorroDB.js
+++ class/CachorroDB.js
@@ -1,5 +1,6 @@
 const Database = require('./Database');
 class CachorroDB {
+    // atributos necessarios para a criação do objeto
     constructor(nome, apelido, anoNascimento, porte, raca, situacao, imagem) {
         this.nome = nome;
         this.apelido = apelido;
@@ -10,12 +11,12 @@ class CachorroDB {
         this.imagem = imagem;
         this.Database = new Database();
     }
-
+    // inserindo no banco de dados
     inserirCachorro(dados, callback) {
         const sql = 'INSERT INTO cachorro (Nome, Id_Raca, Ano_Nascimento, Porte, Adotado, Situacao, Id_Instituicao, Imagem, Apelido) VALUES (?, 1, ?, ?, \'nao\', ?, 1, ?, ?)';
         this.Database.query(sql, [dados.nome, dados.anoNascimento, dados.porte, dados.situacao, dados.imagem, dados.apelido], callback);
     }
-
+    // buscando do banco de dados
     buscarCachorros(callback) {
         const sql = 'SELECT * FROM cachorro';
         this.Database.query(sql, callback);
